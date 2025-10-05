6df5a8d7e6aab4e7af72d75eb404ebb9caa9edca
Terry Mackenzie <tmackenzie26@gmail.com>
2025-10-05 20:03:21 +0100

    Tidy up and stripped out template stuff

--- Models/BatteryMode.cs
+++ Models/BatteryMode.cs
@@ -1,7 +1,12 @@
-﻿namespace BlazorBattControl.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorBattControl.Models
 {
     public class BatteryMode
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public int SchedualId { get; set; }
         public int TimeSlot { get; set; }
--- Models/Schedule.cs
+++ Models/Schedule.cs
@@ -1,7 +1,12 @@
-﻿namespace BlazorBattControl.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorBattControl.Models
 {
     public class Schedule
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public int SchedualId { get; set; }
         public string Name { get; set; }
