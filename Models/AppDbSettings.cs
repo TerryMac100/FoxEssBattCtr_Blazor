7fbfec892eae60a5935d994d6f65db010e739076
Terry Mackenzie <tmackenzie26@gmail.com>
2025-10-25 16:16:01 +0100

    Added settings Db Settings and edit form

--- Models/AppDbSettings.cs
+++ Models/AppDbSettings.cs
@@ -1,7 +1,12 @@
-﻿namespace BlazorBattControl.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorBattControl.Models;
 
 public class AppDbSettings
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
     public int SeletedScheduleId { get; set; }
 
